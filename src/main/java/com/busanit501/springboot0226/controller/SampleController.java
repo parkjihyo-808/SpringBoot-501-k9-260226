42c5d811912e3f2e54800f86dc397d95b83ac6b7
lsy3709 <lsy3709@naver.com>
2026-02-27 11:50:08 +0900

    3교시, 타임리프 기본 문법 서버로부터 전달 받은 다양한 타입의 데이터 클래스 객체들예시10,

--- src/main/java/com/busanit501/springboot0226/controller/SampleController.java
+++ src/main/java/com/busanit501/springboot0226/controller/SampleController.java
@@ -6,11 +6,46 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @Log4j2
 public class SampleController {
+
+    // 내부 클래스로 수업용으로 만듦.
+    class SampleDTO {
+        private String p1,p2,p3;
+
+        public String getP1() {
+            return p1;
+        }
+        public String getP2() {
+            return p2;
+        }
+        public String getP3() {
+            return p3;
+        }
+    }
+
+    @GetMapping("/ex/ex2")
+    public void ex2(Model model) {
+        log.info("/ex/ex2 확인.....");
+        List<String> strList = Arrays.asList("111", "BBB", "CCC");
+        model.addAttribute("strList", strList);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1111");
+        map.put("2", "2222");
+        model.addAttribute("map", map);
+
+        SampleDTO sampleDTO = new SampleDTO();
+        sampleDTO.p1 = "p1 sample value";
+        sampleDTO.p2 = "p2 sample value";
+        sampleDTO.p3 = "p3 sample value";
+        model.addAttribute("sampleDTO", sampleDTO);
+    }
 
     @GetMapping("/hello")
     public void hello(Model model) {
